a370065444882efe5e42443f9c9a580e95f00bad
Arnaud Bergeron <abergeron@gmail.com>
2014-05-06 20:02:38 -0400

    Add a new MISC error type.

--- src/gpuarray_error.c
+++ src/gpuarray_error.c
@@ -20,6 +20,7 @@ const char *gpuarray_error_str(int err) {
   case GA_BLAS_ERROR:        return "Error in BLAS call";
   case GA_UNALIGNED_ERROR:   return "Unaligned array";
   case GA_COPY_ERROR:        return "Copy is needed but disallowed by parameters";
+  case GA_MISC_ERROR:        return "Miscellaneous error";
   default: return "Unknown GA error";
   }
 }
